7ae802da037472c3bf252655881e49c40edf6fbb
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-04-28 08:16:46 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <>
+      <h2>Hello!</h2>
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
